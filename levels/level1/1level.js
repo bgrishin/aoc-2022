3a403514495f27a3867fdc9bad068c52f33bb01f
BOGDAN-GRISHIN <bogdan.grishin.2007@gmail.com>
2023-04-09 09:39:36 -0600

    level 1 part two

--- levels/level1/1level.js
+++ levels/level1/1level.js
@@ -12,4 +12,19 @@ const result = Math.max(
   )
 );
 
-console.log(result);
+console.log(result); // part 1
+
+const result2 = elfCalories
+  .split("\n\n")
+  .map((group) =>
+    group
+      .replaceAll("\n", " ")
+      .split(" ")
+      .map((x) => +x)
+      .reduce((partialSum, a) => partialSum + a, 0)
+  )
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((partialSum, a) => partialSum + a, 0);
+
+console.log(result2); // part 2
