abf5da09dcb67f4559f6f11e8e76025b4569a81b
BOGDAN-GRISHIN <bogdan.grishin.2007@gmail.com>
2023-04-10 22:10:01 -0600

    typo level 6 and level 8

--- levels/level6/6level.js
+++ levels/level6/6level.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const signal = fs.readFileSync("signals.txt", "utf8");
 
 for (let i = 0; i < signal.length; i++) {
-  const peaceOfSignal = signal.slice(i, i + 14);
-  const letters = peaceOfSignal.split("");
+  const pieceOfSignal = signal.slice(i, i + 14); // or 4
+  const letters = pieceOfSignal.split("");
   const uniqueLetters = [...new Set(letters)];
   if (uniqueLetters.length === letters.length) {
-    console.log(peaceOfSignal, i + 14);
+    console.log(pieceOfSignal, i + 14); // part 1 and 2 (14 or 4)
     break;
   }
 }
